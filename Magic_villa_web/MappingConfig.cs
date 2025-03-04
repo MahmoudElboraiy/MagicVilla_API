b9ca55658296bb13048d34615851a808107d3c69
mahmoud-elboraiy <99480478+mahmoud-elboraiy@users.noreply.github.com>
2025-03-04 03:23:22 +0200

    autoMapper in web

---
+++ Magic_villa_web/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Magic_villa_web.DTO;
+using Magic_villa_web.Models.DTO;
+
+
+namespace Magic_villa_web
+{
+    public class MappingConfig:Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
