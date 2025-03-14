9724506a9be3ca2b6582fa44ef001acaa59f7669
mahmoud-elboraiy <99480478+mahmoud-elboraiy@users.noreply.github.com>
2025-03-14 15:10:21 +0200

    Login implment

---
+++ MagicVilla_API/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_API.Models;
+using MagicVilla_API.Models.DTO;
+
+namespace MagicVilla_API.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        public bool IsUnique(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
