521570fe78f15fa525cdf27db93a7f6743cb467e
mahmoud-elboraiy <99480478+mahmoud-elboraiy@users.noreply.github.com>
2025-03-14 15:24:18 +0200

    User Controller

---
+++ MagicVilla_API/Controllers/UsersController.cs
@@ -0,0 +1,64 @@
+﻿using MagicVilla_API.Models;
+using MagicVilla_API.Models.DTO;
+using MagicVilla_API.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/UsersAuth")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+
+        private readonly IUserRepository _userRepo;
+        protected APIResponse _response;
+        public UsersController(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;
+            this._response = new();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO model)
+        {
+            var loginResponse = await _userRepo.Login(model);
+            if (loginResponse.User == null || string.IsNullOrEmpty(loginResponse.Token))
+            {
+                _response.HttpStatusCode = HttpStatusCode.BadRequest;
+                _response.IsSucessed = false;
+                _response.ErrorMessages.Add("Username or password is incorrect");
+                return BadRequest(_response);
+            }
+            _response.HttpStatusCode = HttpStatusCode.OK;
+            _response.IsSucessed = true;
+            _response.Result = loginResponse;
+            return Ok(_response);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterationRequestDTO model)
+        {
+            bool ifUserNameUnique = _userRepo.IsUnique(model.UserName);
+            if (!ifUserNameUnique)
+            {
+                _response.HttpStatusCode = HttpStatusCode.BadRequest;
+                _response.IsSucessed = false;
+                _response.ErrorMessages.Add("Username already exists");
+                return BadRequest(_response);
+            }
+
+            var user = await _userRepo.Register(model);
+            if (user == null)
+            {
+                _response.HttpStatusCode = HttpStatusCode.BadRequest;
+                _response.IsSucessed = false;
+                _response.ErrorMessages.Add("Error while registering");
+                return BadRequest(_response);
+            }
+            _response.HttpStatusCode = HttpStatusCode.OK;
+            _response.IsSucessed = true;
+            return Ok(_response);
+        }
+    }
+}
--- MagicVilla_API/Models/APIResponse.cs
+++ MagicVilla_API/Models/APIResponse.cs
@@ -4,6 +4,10 @@ namespace MagicVilla_API.Models
 {
     public class APIResponse
     {
+        public APIResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
         public HttpStatusCode HttpStatusCode { get; set; }
         public bool IsSucessed { get; set; } = true;
         public List<string> ErrorMessages { get; set; }
