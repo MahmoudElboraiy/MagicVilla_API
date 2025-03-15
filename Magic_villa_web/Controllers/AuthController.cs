9f0746bd08265b40579aeb43a1685d56b7294d51
mahmoud-elboraiy <99480478+mahmoud-elboraiy@users.noreply.github.com>
2025-03-15 17:54:51 +0200

    Auth Controller Action Methods

--- Magic_villa_web/Controllers/AuthController.cs
+++ Magic_villa_web/Controllers/AuthController.cs
@@ -1,7 +1,10 @@
 ﻿using Magic_villa_web.DTO;
 using Magic_villa_web.Models;
 using Magic_villa_web.Services.IServices;
+using MagicVilla_utility;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Magic_villa_web.Controllers
 {
@@ -24,7 +27,18 @@ namespace Magic_villa_web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginRequestDTO obj)
         {
-            return View();
+            APIResponse response = await _authService.LoginAsync<APIResponse>(obj);
+            if (response != null && response.IsSucessed)
+            {
+                LoginResponseDTO model = JsonConvert.DeserializeObject<LoginResponseDTO>(Convert.ToString(response.Result));
+                HttpContext.Session.SetString(SD.SessionToken, model.Token);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", response.ErrorMessages.FirstOrDefault());
+                return View(obj);
+            }
         }
 
         [HttpGet]
@@ -49,7 +63,9 @@ namespace Magic_villa_web.Controllers
         [HttpGet]
         public async Task<IActionResult> Logout()
         {
-            return View();
+            await HttpContext.SignOutAsync();
+            HttpContext.Session.SetString(SD.SessionToken, "");
+            return RedirectToAction("Index", "Home");
         }
         [HttpGet]
         public IActionResult AccessDenied()
