31a31b6f4ae84af19692ff209e225ca552a08122
mahmoud-elboraiy <99480478+mahmoud-elboraiy@users.noreply.github.com>
2025-03-15 15:26:30 +0200

    Auth Controller Action Methods

---
+++ Magic_villa_web/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using Magic_villa_web.DTO;
+using Magic_villa_web.Models;
+using Magic_villa_web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Magic_villa_web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO obj = new();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginRequestDTO obj)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterationRequestDTO obj)
+        {
+            APIResponse result = await _authService.RegisterAsync<APIResponse>(obj);
+            if (result != null && result.IsSucessed)
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
